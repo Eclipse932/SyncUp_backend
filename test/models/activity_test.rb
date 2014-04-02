1bc1f5b7f6a5afda24a379805e6fb2aacbcf241e
Shiyun Huang <shiyunh2011@gmail.com>
2014-04-02 14:31:48 -0700

    local change saved

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -4,6 +4,9 @@ class ActivityTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  user = User.new(:email => "u1@example.com", :password => "p1", :password_confirmation => "p1", :first_name  => "Jane", :last_name => "Huang", :description => "a female code monkey")
-	  
+  test "validate activity name is not nil" do 
+  	
+  end
+
+
 end
