114da17162745dc4973a8843b4fa28db7b1b3c70
Andrew Chang <andrewgchang@gmail.com>
2014-04-03 02:23:58 -0700

    Added phone_number column to users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,27 +5,27 @@ class UsersController < ApplicationController
 
 
 	def create
-    permitted = params.require(:user).permit(:email, :first_name, :last_name, :description, :avatar,
-    																				 :password, :password_confirmation)
-    user = User.new(permitted)
-    if user.save
-    	if user == nil
-    		puts "wtf is this nil"
-    	end
-      sign_in user
-      render :status => 200,
-           :json => { :success => true,
-                      :info => "Registered",
-                      :data => { :auth_token => current_user.authentication_token,
-                                 :email => current_user.email,
-                                 :user => user} }
-    else
-      render :status => :unprocessable_entity,
-             :json => { :success => false,
-                        :info => user.errors,
-                        :data => {} }
+        permitted = params.require(:user).permit(:email, :first_name, :last_name, :description, :avatar,
+                                                                                         :password, :password_confirmation)
+        user = User.new(permitted)
+        if user.save
+            if user == nil
+                puts "wtf is this nil"
+            end
+          sign_in user
+          render :status => 200,
+               :json => { :success => true,
+                          :info => "Registered",
+                          :data => { :auth_token => current_user.authentication_token,
+                                     :email => current_user.email,
+                                     :user => user} }
+        else
+          render :status => :unprocessable_entity,
+                 :json => { :success => false,
+                            :info => user.errors,
+                            :data => {} }
+        end
     end
-  end
 		
 
 	def searchUser
